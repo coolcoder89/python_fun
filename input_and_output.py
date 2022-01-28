20cf643893efcd37025ae3b8bd51eeef237aeeb4
rishan <97000520+coolcoder89@users.noreply.github.com>
2022-01-27 19:23:28 -0600

    Update input_and_output.py

--- input_and_output.py
+++ input_and_output.py
@@ -1,4 +1,5 @@
-n1 == input("hello,what is your first name?")
-n2 == input("what is your last name?")
+n1 = input("hello,what is your first name?")
+n2 = input("what is your last name?")
 
 print("your first name is",n2,"your last name is",n2)
+//prints first name and last name
